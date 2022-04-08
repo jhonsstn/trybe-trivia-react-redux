add53837c3dc31790b46c69c5c3d272f8f3edfd7
Jhon Santana <jhonsstn@gmail.com>
2022-04-08 18:05:43 -0300

    feat(Ranking.js): requisito 18 completo
    Adicionada logica de adicao dos players ao localStorage e de listagem dos clientes em ordem na tela de Ranking.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,6 +5,24 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  componentDidMount() {
+    const { name, score, avatar } = this.props;
+    const playerData = {
+      name,
+      score,
+      avatar,
+    };
+
+    if (localStorage.getItem('ranking')) {
+      const data = JSON.parse(localStorage.getItem('ranking'));
+      data.push(playerData);
+      localStorage.setItem('ranking', JSON.stringify(data));
+    } else {
+      const array = [playerData];
+      localStorage.setItem('ranking', JSON.stringify(array));
+    }
+  }
+
   render() {
     const { assertions, score } = this.props;
     return (
@@ -36,12 +54,16 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
   score: state.player.score,
+  name: state.player.name,
+  avatar: state.player.avatar,
 });
 
 export default connect(mapStateToProps)(Feedback);
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    const storage = JSON.parse(localStorage.getItem('ranking'));
+
+    const ranking = storage.sort(this.compareScore);
+
+    this.setState({
+      ranking,
+    });
+  }
+
+  compareScore = (a, b) => b.score - a.score;
+
   render() {
+    const { ranking } = this.state;
     return (
       <>
         <h3 data-testid="ranking-title">Ranking</h3>
+        {ranking.map((player, index) => (
+          <div key={ player.name }>
+            <span data-testid={ `player-name-${index}` }>{player.name}</span>
+            <span data-testid={ `player-score-${index}` }>{player.score}</span>
+          </div>
+        ))}
         <Link to="/">
           <input data-testid="btn-go-home" type="button" value="Login" />
         </Link>
